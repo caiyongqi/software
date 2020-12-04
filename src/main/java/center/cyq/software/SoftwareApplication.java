e5da171e75f85179aacea04444fda8a349fa04eb
caiyongqi <1592341907@qq.com>
2020-12-04 23:20:24 +0800

    ''

--- src/main/java/center/cyq/software/SoftwareApplication.java
+++ src/main/java/center/cyq/software/SoftwareApplication.java
@@ -1,30 +1,14 @@
 package center.cyq.software;
 
-import org.springframework.beans.factory.ListableBeanFactory;
-import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.http.HttpMessageConverters;
-import org.springframework.boot.autoconfigure.web.ResourceProperties;
-import org.springframework.boot.autoconfigure.web.servlet.DispatcherServletPath;
-import org.springframework.boot.autoconfigure.web.servlet.WebMvcAutoConfiguration;
-import org.springframework.boot.autoconfigure.web.servlet.WebMvcProperties;
-import org.springframework.boot.web.servlet.ServletRegistrationBean;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
+
 
 @SpringBootApplication
-public class SoftwareApplication extends WebMvcAutoConfiguration.WebMvcAutoConfigurationAdapter {
-
-    public SoftwareApplication(ResourceProperties resourceProperties, WebMvcProperties mvcProperties, ListableBeanFactory beanFactory, ObjectProvider<HttpMessageConverters> messageConvertersProvider, ObjectProvider<WebMvcAutoConfiguration.ResourceHandlerRegistrationCustomizer> resourceHandlerRegistrationCustomizerProvider, ObjectProvider<DispatcherServletPath> dispatcherServletPath, ObjectProvider<ServletRegistrationBean<?>> servletRegistrations) {
-        super(resourceProperties, mvcProperties, beanFactory, messageConvertersProvider, resourceHandlerRegistrationCustomizerProvider, dispatcherServletPath, servletRegistrations);
-    }
+public class SoftwareApplication{
 
     public static void main(String[] args) {
         SpringApplication.run(SoftwareApplication.class, args);
     }
 
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowCredentials(true).allowedHeaders("*").allowedOrigins("*").allowedMethods("*");
-    }
 }
