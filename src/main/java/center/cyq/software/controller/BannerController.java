92b2552a3ba928b1fc0cc56556436a56d8478e58
caiyongqi <1592341907@qq.com>
2020-12-08 19:00:00 +0800

    ''

--- src/main/java/center/cyq/software/controller/BannerController.java
+++ src/main/java/center/cyq/software/controller/BannerController.java
@@ -4,9 +4,11 @@ import center.cyq.software.service.BannerService;
 import net.sf.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+@CrossOrigin(allowCredentials="true")
 @Controller
 public class BannerController {
     private BannerService bannerService;
